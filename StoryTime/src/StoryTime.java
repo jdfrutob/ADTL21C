2b987268261d0caf558070a20f63022eb7c9be46
JD Biñegas <john.binegas@lpunetwork.edu.ph>
2023-09-19 15:32:16 +0800

    StoryTime

---
+++ StoryTime/src/StoryTime.java
@@ -0,0 +1,12 @@
+public class StoryTime {
+    public static void main(String[] args) {
+
+        String story = "Today, we were tasked with creating two Java projects for our ADTL21C subject.";
+        story += "One project involved building a basic calculator in Java, while the other required us to craft a story.";
+        story += "For this particular assignment, we were given the following instructions:";
+        story += "Create a Java program that will narrate a story.";
+        story += "So, here's my story:";
+
+        System.out.println(story);
+    }
+}

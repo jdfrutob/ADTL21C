7c2e404defed5f9bb483dcdbd4c9e275073787bb
JD Biñegas <129596870+jdfrutob@users.noreply.github.com>
2023-12-26 20:09:43 +0800

    DELETE

---
+++ try.java
@@ -0,0 +1,7 @@
+// Example
+public class Main {
+    Product p1 = new Product("Flava Disposable",500);
+    Product p2 = new Product("Aegis Disposable",550);
+    Product p3 = new Product("Cyclone Disposable",600);
+    
+}
